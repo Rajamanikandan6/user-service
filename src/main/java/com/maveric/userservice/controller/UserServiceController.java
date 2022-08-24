a3c0aceca7f8dd0da7131a758aa0f9138bad8f31
Rajamanikandan Soliappan <rajamanikandanso@maveric-systems.com>
2022-08-24 20:17:13 +0530

    initial commit

--- src/main/java/com/maveric/userservice/controller/UserServiceController.java
+++ src/main/java/com/maveric/userservice/controller/UserServiceController.java
@@ -1,5 +1,26 @@
 package com.maveric.userservice.controller;
 
-public class UserServiceController {
+import com.maveric.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.maveric.userservice.model.User;
 
+@RestController
+public class UserServiceController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        user.setPassword(this.bCryptPasswordEncoder.encode(user.getPassword()));
+        User userDetails = userService.createUserDetails(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDetails);
+    }
 }
--- src/main/java/com/maveric/userservice/service/UserService.java
+++ src/main/java/com/maveric/userservice/service/UserService.java
@@ -1,4 +1,18 @@
 package com.maveric.userservice.service;
 
+import com.maveric.userservice.model.User;
+import com.maveric.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User createUserDetails(User user){
+        return userRepository.save(user);
+    }
+
 }
