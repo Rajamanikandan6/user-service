23758537cd3e87e8776769e64770593457a24a38
Rajamanikandan Soliappan <rajamanikandanso@maveric-systems.com>
2022-08-24 22:46:39 +0530

    added get userbyid functionality

--- src/main/java/com/maveric/userservice/service/UserService.java
+++ src/main/java/com/maveric/userservice/service/UserService.java
@@ -1,4 +1,18 @@
 package com.maveric.userservice.service;
 
+import com.maveric.userservice.model.User;
+import com.maveric.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public Optional<User> getUserDetails(int userId){
+        return userRepository.findById(userId);
+    }
 }
