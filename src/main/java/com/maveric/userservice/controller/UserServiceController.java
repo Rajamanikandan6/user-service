4380bd8d508a07c8070e4db5ee6ab6c51c749d00
Rajamanikandan Soliappan <rajamanikandanso@maveric-systems.com>
2022-08-25 09:17:01 +0530

    added user update functionality

--- src/main/java/com/maveric/userservice/controller/UserServiceController.java
+++ src/main/java/com/maveric/userservice/controller/UserServiceController.java
@@ -1,5 +1,24 @@
 package com.maveric.userservice.controller;
 
+import com.maveric.userservice.model.User;
+import com.maveric.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserServiceController {
 
+    @Autowired
+    UserService userService;
+
+    @PutMapping("/users/{userId}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable int userId){
+        User userDetails = userService.getUserDetails(user,userId);
+        return ResponseEntity.status(HttpStatus.OK).body(userDetails);
+    }
 }
--- src/main/java/com/maveric/userservice/service/UserService.java
+++ src/main/java/com/maveric/userservice/service/UserService.java
@@ -1,4 +1,32 @@
 package com.maveric.userservice.service;
 
+import com.maveric.userservice.model.User;
+import com.maveric.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public User getUserDetails(User user,int userId){
+        Optional<User> userFromDb = userRepository.findById(userId);
+        if(userFromDb.isPresent()) {
+            User newUser = userFromDb.get();
+            newUser.setFirstName(user.getFirstName());
+            newUser.setLastName(user.getLastName());
+            newUser.setMiddleName(user.getMiddleName());
+            newUser.setPhoneNumber(user.getPhoneNumber());
+            newUser.setEmail(user.getEmail());
+            newUser.setAddress(user.getAddress());
+            newUser.setDateOfBirth(user.getDateOfBirth());
+            newUser.setGender(user.getGender());
+
+            return userRepository.save(newUser);
+        }
+        return null;
+    }
 }
